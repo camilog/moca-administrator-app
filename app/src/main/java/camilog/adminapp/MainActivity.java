c0d672f59282f7d28e2553e844846a6af8e1e416
Stefano Gioia <stefanogch@gmail.com>
2015-09-02 12:18:37 -0300

    creados layouts configuracion y de nueva elecci'on

--- app/src/main/java/camilog/adminapp/MainActivity.java
+++ app/src/main/java/camilog/adminapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.election_layout);
     }
 
     @Override

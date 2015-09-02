5c67309e89acdd4d9f4587785d1b6607d98e2afe
Stefano Gioia <stefanogch@gmail.com>
2015-09-02 11:34:25 -0300

    credao layout para control de elecci'on en particular

---
+++ app/src/main/java/camilog/adminapp/serverapi/BBServer.java
@@ -0,0 +1,11 @@
+package camilog.adminapp.serverapi;
+
+/**
+ * Created by stefano on 02-09-15.
+ */
+public class BBServer {
+    private String _serverAddress;
+    public BBServer(String address){
+        _serverAddress = address;
+    }
+}
